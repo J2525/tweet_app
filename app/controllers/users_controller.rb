f4b059333736d8bf22e0cd22997a76c18fe8beee
J2525 <blue.east1028@gmail.com>
2019-01-25 00:55:16 +0900

    ユーザー新規登録機能を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.order(created_at: :desc)
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  # createアクションを作成してください
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    @user.save
+    redirect_to("/users/#{@user.id}")
   end
 end
