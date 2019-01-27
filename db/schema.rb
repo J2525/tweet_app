835ae28a17049b542a04e59c9cc4cc9002744b97
J2525 <blue.east1028@gmail.com>
2019-01-27 12:46:28 +0900

    $ rails g migration add_image_name_to_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190123141932) do
+ActiveRecord::Schema.define(version: 20190127034104) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20190123141932) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
